65f4c69aaf79e39637bf8592a0946e0ef57c370d
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-06 11:18:24 -0400

    GameDelete made in controller

--- Game_Finder_APi/Controllers/GameController.cs
+++ Game_Finder_APi/Controllers/GameController.cs
@@ -4,10 +4,28 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 
 namespace Game_Finder_APi.Controllers
 {
     public class GameController : ApiController
     {
+        //Create Game Service
+        private GameService CreateGameService()
+        {
+            var ownerId = Guid.Parse(User.Identity.GetUserId());
+            var gameService = new GameService(ownerId);
+            return gameService;
+        }
+        //Delete by Id
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateGameService();
+            if (!service.DeleteGame(id))
+            {
+                return InternalServerError();
+            }
+            return Ok();
+        }
     }
 }
